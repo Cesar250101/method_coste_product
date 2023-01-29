aadb931cd47ee7d131da51d2f67e9d1c3ba7b254
Cesar250101 <cesar250101@outlook.com>
2023-01-29 14:45:42 -0300

    first commit

--- models/models.py
+++ models/models.py
@@ -5,14 +5,14 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.product'
 
-    coste = fields.Float(compute='_compute_costo', string='Costo Producto',store=True)
+    costo_producto = fields.Float(compute='_compute_costo', string='Costo Producto',store=True)
     
     @api.depends('stock_move_ids','stock_quant_ids')
     def _compute_costo(self):
         for p in self:
             coste=self.env['product.price.history'].search([('product_id','=',p.id)],order="datetime desc",limit=1)
             if coste:
-                p.coste=coste.cost
+                p.costo_producto=coste.cost
 
     @api.model
     def _obtener_costo_producto(self):
@@ -20,5 +20,5 @@ class ProductTemplate(models.Model):
         for p in productos:
             coste=self.env['product.price.history'].search([('product_id','=',p.id)],order="datetime desc",limit=1)
             if coste and coste.cost>0:
-                p.coste=coste.cost
+                p.costo_producto=coste.cost
 
